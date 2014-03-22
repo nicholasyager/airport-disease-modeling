49ba241243e27c67badb46ae199c2be398e5cdca
Nicholas Yager <yager@nicholasyager.com>
2014-03-21 23:20:36 -0400

    Started analyzing data for the cluster simulations with a 10 day delay

--- scripts/ANOVA.r
+++ scripts/ANOVA.r
@@ -2,35 +2,44 @@ library(plotrix)
 library(agricolae)
 
 R <- as.data.frame(read.csv("random.matrix",header=F))
-#D <- as.data.frame(read.csv("degree.matrix",header=F))
+D <- as.data.frame(read.csv("weight.matrix",header=F))
 B <- as.data.frame(read.csv("betweenness.matrix",header=F))
+C <- as.data.frame(read.csv("cluster.matrix",header=F))
 
 r <- data.frame()
 k = 1
 
-efforts = c(1,3,5,7,9)
+efforts = c(1,3,7,11,17)
 
 for (i in efforts) {
   for (j in 1:length(R[,1])) {
-    r[k,1] <- 1
+    r[k,1] <- "Random"
     r[k,2] <- as.character((i-1)*5)
     r[k,3] <- R[j,i]
     k = k + 1
   }
 }
-# for (i in c(1,3,5,7,8)) {
-#   for (j in 1:50) {
-#     r[k,1] <- 2
-#     r[k,2] <- as.character((i-1)*5)
-#     r[k,3] <- D[j,i]
-#     k = k + 1
-#   }
-# }
+for (i in efforts) {
+  for (j in 1:50) {
+    r[k,1] <- "Weight"
+    r[k,2] <- as.character((i-1)*5)
+    r[k,3] <- D[j,i]
+    k = k + 1
+  }
+}
 for (i in efforts) {
   for (j in 1:length(B[,1])) {
-    r[k,1] <- 3
+    r[k,1] <- "Betweenness"
     r[k,2] <- as.character((i-1)*5)
     r[k,3] <- B[j,i]
+    k = k + 1
+  }
+}
+for (i in efforts) {
+  for (j in 1:length(B[,1])) {
+    r[k,1] <- "Cluster"
+    r[k,2] <- as.character((i-1)*5)
+    r[k,3] <- C[j,i]
     k = k + 1
   }
 }
@@ -54,24 +63,29 @@ tx <- with(data.stacked, interaction(effort, qtype))
 data <- HSD.test(data.aov.tk, "tx" ,group=T)
 
 M <- tapply(data.stacked$infected,list(data.stacked$qtype, data.stacked$effort),median)
-M <- M[,order(M[2,],decreasing=T)]
+#M <- M[,order(M[2,],decreasing=T)]
 
 CI95 = qt(1-0.05/2,9)*(sd(data.stacked$infected)/sqrt(length(data.stacked$infected))) / 3287
 
-bp = barplot(M/3287 ,beside=T,
+colors <- c("gray20",
+            "gray40",
+            "gray60",
+            "gray80")
+
+bp = barplot(M/3287 ,beside=T, col=colors,
              xlab="Quarantine Effort (% of edges closed)", 
              ylab="Proportion of infected airports",
-             main="Comparison of Quarantine Strategies",
+             main="Comparison of Quarantine Strategies with 10 Day Delay",
              ylim=c(0,1)
 )
-legend(x=12,y=1, legend=c("Random","Betweenness"), fill=c("gray30","gray90"), cex=0.75)
+legend("topright", legend=rownames(M), fill=colors, cex=0.8)
 plotCI(bp, M/3287, CI95, add=T, pch=NA)
-text(x=as.vector(bp),y=as.vector(M/3287)+CI95,c("A","A",
-                                           "B","C",
-                                           "D","E",
-                                           "F","G",
-                                           "H","I"
-                                           ), pos=3,cex=0.8)
+text(x=as.vector(bp),y=as.vector(M/3287)+CI95,c("A","A","A","A",
+                                                "B","C","D","E",
+                                                "F","G","H","I",
+                                                "J","J","K","L",
+                                                "M","N","O","P"
+                                                ), pos=3,cex=0.8)
 
 abline(h=0)
 
